27fa446f91e8bd68fa32692ead31c7fb5dc396af
linedev06 <vitorafelismino@gmail.com>
2023-03-31 19:27:51 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Qual é o seu nome?");
+string nome = Console.ReadLine()!;
+
+Console.Write("Qual é o seu sobrenome?");
+string sobrenome = Console.ReadLine()!;
+
+Console.WriteLine(    $"boa noite, {nome} {sobrenome}");
